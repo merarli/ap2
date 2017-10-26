01f50489041b9676c3d1fb267b8cdcc088e2ed49
merarli <merarli@192.168.10.4>
2017-10-26 18:31:23 +0900

    遅れて課題４追加

--- src/pkg20171026/Kadai04_4.java
+++ src/pkg20171026/Kadai04_4.java
@@ -36,10 +36,12 @@ public class Kadai04_4 extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jButton2 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("検索");
+        jButton1.setText("計算");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -50,27 +52,41 @@ public class Kadai04_4 extends javax.swing.JFrame {
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
+        jButton2.setText("検索");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(159, 159, 159)
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addContainerGap(28, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(26, 26, 26)
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1)
-                .addGap(32, 32, 32)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(86, Short.MAX_VALUE))
+                .addContainerGap(74, Short.MAX_VALUE))
         );
 
         pack();
@@ -91,7 +107,7 @@ public class Kadai04_4 extends javax.swing.JFrame {
             Statement stmt = con.createStatement();
 
             //実行するSQL文を記述
-            String sql = "select min(score) as G_MIN, avg(score) as G_AVG, max(score) as G_MAX from T_STUDENT group by gakubu_id";
+            String sql = "select gakubu_name, min(score) as g_min, avg(score) as g_avg, max(score) as g_max, count(*) as g_count from T_STUDENT INNER JOIN T_GAKUBU ON T_STUDENT.GAKUBU_ID = T_GAKUBU.GAKUBU_ID group by GAKUBU_name";
 
             //SQLを実行してResultSetの形式で結果を取得
             ResultSet rs = stmt.executeQuery(sql);
@@ -103,13 +119,14 @@ public class Kadai04_4 extends javax.swing.JFrame {
             //取得したレコードを一つずつ処理
             while (rs.next()) {
                 String result = "";
-                result += (count) + ":";
-                result += rs.getInt("g_min")+ ",";
+                result += rs.getString("gakubu_name") + ",";
+                result += rs.getInt("g_min") + ",";
                 result += rs.getInt("g_avg") + ",";
                 result += rs.getInt("g_max") + ",";
+                result += rs.getInt("g_count") + ",";
                 
-                jTextArea1.append(result + "\n");
-                count++;
+
+              jTextArea1.append(result + "\n");
                 
             }
             //メモリの開放
@@ -120,6 +137,49 @@ public class Kadai04_4 extends javax.swing.JFrame {
             ex.printStackTrace();
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        
+        try {
+        String driverUrl2 = "jdbc:derby://localhost:1527/meibo";
+        
+        Connection con
+                    = DriverManager.getConnection(driverUrl2,"db","db");
+        
+        Statement stmt2 = con.createStatement();
+         String sql2 = "select * from T_STUDENT where GAKUBU_ID = "+jTextField1.getText();
+
+            //SQLを実行してResultSetの形式で結果を取得
+            ResultSet rs = stmt2.executeQuery(sql2);
+            
+            
+            jTextArea1.setText("");
+            jTextArea1.append("学籍番号,氏名,学部ID,学年,スコア"+"\n");
+
+            //取得したレコードを一つずつ処理
+            while (rs.next()) {
+                String result = "";
+                result += rs.getInt("student_id") + ",";
+                result += rs.getString("fullname")+ ",";
+                result += rs.getString("gakubu_id") + ",";
+                result += rs.getString("grade") + ",";
+                result += rs.getString("score") ;
+                
+                jTextArea1.append(result + "\n");
+                
+            }
+            
+                        //メモリの開放
+            rs.close();
+            stmt2.close();
+            con.close();
+             } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,7 +225,9 @@ public class Kadai04_4 extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
